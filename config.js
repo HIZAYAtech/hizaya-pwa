1b2c02e35ff3288b6041f575b4a6aac64aa89855
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-09-25 18:27:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-// REMPLACE par ton projet Supabase
-window.CONFIG = {
-  SUPABASE_URL:  "https://ctjljqmxjnfykskfgral.supabase.co", // ← à coller
-  SUPABASE_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0amxqcW14am5meWtza2ZncmFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0NzQzMDMsImV4cCI6MjA3MjA1MDMwM30.GoLM8CSHRh2KWOmrMLk2-JkFMz2hwAqyHaHxd8T51M4",                  // ← à coller
-  // Broker WSS par défaut (tu peux changer ici ou dans le champ UI)
-  BROKER_WSS: "broker.hivemq.com:8884/mqtt"
-};
+export const SUPABASE_URL = 'https://zeknluovgbsmtgupwkvo.supabase.co';   // Settings → API → Project URL
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpla25sdW92Z2JzbXRndXB3a3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MjY2NjIsImV4cCI6MjA3NDMwMjY2Mn0.AjCTWEAGBBhi5liJv8LBCK_baqivRlDIYMbFcDeA_q4';                  // Settings → API → anon public
+export const REDIRECT_URL = 'https://hizayatech.github.io/hizaya-pwa/'; // ton URL Pages (avec le / final)
