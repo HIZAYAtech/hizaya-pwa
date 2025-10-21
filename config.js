f12cbf4bb1cffbc4dd871b411d0397563991ab11
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-21 23:48:01 +0200

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://zeknluovgbsmtgupwkvo.supabase.co';   // Settings → API → Project URL
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpla25sdW92Z2JzbXRndXB3a3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MjY2NjIsImV4cCI6MjA3NDMwMjY2Mn0.AjCTWEAGBBhi5liJv8LBCK_baqivRlDIYMbFcDeA_q4';                  // Settings → API → anon public
-export const REDIRECT_URL = 'https://hizayatech.github.io/hizaya-pwa/'; // ton URL Pages (avec le / final)
