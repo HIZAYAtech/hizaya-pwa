f855c0231ce376526416ed4dbf6e990b59c5deaa
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-22 03:42:30 +0200

    Update vite.config.js

--- web/app/vite.config.js
+++ web/app/vite.config.js
@@ -1,24 +1,9 @@
-// web/app/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ⚠️ IMPORTANT (GitHub Pages):
-// Si ton site est servi sous https://<user>.github.io/<repo>/
-// alors `base` doit être "/<repo>/". Ici: "/hizaya-pwa/"
-// Si tu déploies à la racine d'un domaine (ex: Netlify), mets simplement: base: "/"
 export default defineConfig({
   plugins: [react()],
-  base: '/hizaya-pwa/',
-
-  // Options utiles (tu peux les laisser telles quelles ou ajuster)
-  build: {
-    outDir: 'dist',
-    sourcemap: false, // passe à true si tu veux débugger en prod
-    target: 'esnext',
-  },
-  server: {
-    port: 5173,
-    host: true,
-    open: true,
-  },
+  base: '/hizaya-pwa/', // mets le nom EXACT du repo entre les deux "/" s'il diffère
+  build: { outDir: 'dist', target: 'esnext' },
+  server: { host: true, port: 5173, open: true },
 });
