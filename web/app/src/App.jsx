6f448208d35686fd43cc334e655e3c9acae8e6d7
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-27 14:42:59 +0100

    Update App.jsx

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -16,7 +16,7 @@ const DEFAULT_IO_PIN = 26;
 
 /* Image de fond (peut être changée librement) */
 const BG_URL =
-  "https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&w=1600&q=60";
+  "https://blackstone.scene7.com/is/image/blackstone/alex-mNJ7c7-XCZQ-unsplash_editted?wid=1280&hei=853";
 
 /* =========================
    STYLES GLOBAUX
