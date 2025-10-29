4bc8b4300f66c6364233bec3182f3b3e21fc3d96
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-29 22:27:38 +0100

    Update App.jsx

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -3,12 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 /* =========================================
    CONFIG SUPABASE
-   ========================================= */
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "";
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
+
     autoRefreshToken: true,
     detectSessionInUrl: true,
   },
