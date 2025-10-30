591aaa53b8bcdfcae3401bb5867046a4157e8d22
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-30 23:19:51 +0100

    Update main.jsx

--- web/app/src/main.jsx
+++ web/app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./styles.css";
 
-const el = document.getElementById("root");
-createRoot(el).render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
