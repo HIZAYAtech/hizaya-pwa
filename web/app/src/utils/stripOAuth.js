7661e1a6920b007c2aa13059803d528781515c9c
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-30 23:49:45 +0100

    Create stripOAuth.js

---
+++ web/app/src/utils/stripOAuth.js
@@ -0,0 +1,20 @@
+// src/utils/stripOAuth.js
+export function stripOAuthParams() {
+  try {
+    const url = new URL(window.location.href);
+    let changed = false;
+    ["code", "state", "provider", "error", "error_description"].forEach((p) => {
+      if (url.searchParams.has(p)) {
+        url.searchParams.delete(p);
+        changed = true;
+      }
+    });
+    if (url.hash && /access_token|refresh_token|error/i.test(url.hash)) {
+      url.hash = "";
+      changed = true;
+    }
+    if (changed) {
+      window.history.replaceState({}, document.title, url.toString());
+    }
+  } catch {}
+}
