308ea17e8090de61ab165b4bee44fe4e0ae7479a
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-31 15:58:55 +0100

    Update vite.config.js

--- web/app/vite.config.js
+++ web/app/vite.config.js
@@ -1,20 +1,7 @@
-// web/app/vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Chemins RELATIFS -> parfait pour GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: './',                 // <<< clé : plus de chemins absolus /
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',      // JS/CSS sous dist/assets
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
-      }
-    }
-  }
-})
+  base: "/hizaya-pwa/", // nom du repo GitHub Pages
+});
