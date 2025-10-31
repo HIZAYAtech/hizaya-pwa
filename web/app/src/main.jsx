415098336b9b1a25fb8b079bbe99499433660104
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-31 11:36:58 +0100

    Update main.jsx

--- web/app/src/main.jsx
+++ web/app/src/main.jsx
@@ -1,17 +1,15 @@
-// src/main.jsx--
+// web/app/src/main.jsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
 import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/app" element={<Dashboard />} />
-      {/* par défaut, on redirige vers /app */}
-      <Route path="*" element={<Navigate to="/app" replace />} />
-    </Routes>
-  </BrowserRouter>
+const root = document.getElementById("root");
+createRoot(root).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
