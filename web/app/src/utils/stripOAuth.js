cd0bd2b23d28ec9a43b5c21894c46e2da4dbed87
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-31 16:04:38 +0100

    Update stripOAuth.js

--- web/app/src/utils/stripOAuth.js
+++ web/app/src/utils/stripOAuth.js
@@ -1,26 +1,22 @@
-// Nettoie ?code=... & ?state=... et les fragments d'erreur, tout en conservant le #/route
-export default function stripOAuth() {
+export default function stripOAuthParams() {
   try {
     const url = new URL(window.location.href);
     let changed = false;
 
-    ["code", "state", "error", "error_description", "provider"].forEach((p) => {
+    ["code", "state", "provider", "error", "error_description"].forEach((p) => {
       if (url.searchParams.has(p)) {
         url.searchParams.delete(p);
         changed = true;
       }
     });
 
-    // si des tokens sont dans le hash (rare en PKCE), on les enlève
-    if (url.hash && /(access_token|refresh_token|error)=/i.test(url.hash)) {
-      // on conserve uniquement la partie route avant le '?'
-      const frag = url.hash.split("?")[0];
-      url.hash = frag;
+    if (url.hash && /access_token|refresh_token|error/i.test(url.hash)) {
+      url.hash = "";
       changed = true;
     }
 
-    if (changed) {
-      window.history.replaceState({}, document.title, url.toString());
-    }
-  } catch (_) {}
+    if (changed) window.history.replaceState({}, document.title, url.toString());
+  } catch {
+    /* noop */
+  }
 }
