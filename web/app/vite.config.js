a063b4f26062682466df9e623d6b6dfff371b875
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-31 14:39:16 +0100

    Update vite.config.js

--- web/app/vite.config.js
+++ web/app/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Pour GitHub Pages (project pages)
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+// Remplace par le nom exact de ton repo GitHub Pages
+const REPO = "hizaya-pwa";
+
 export default defineConfig({
+  base: `/${REPO}/web/app/`, // <- si tu sers depuis /hizaya-pwa/web/app/
   plugins: [react()],
-  base: repo ? `/${repo}/` : "/"
 });
