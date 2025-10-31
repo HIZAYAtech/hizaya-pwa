bc4a44184e9ae9b674038cc1225e519b31feacf2
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-10-31 14:10:46 +0100

    Update stripOAuth.js

--- web/app/src/utils/stripOAuth.js
+++ web/app/src/utils/stripOAuth.js
@@ -1,14 +1,15 @@
-// src/utils/stripOAuth.js------
-export function stripOAuthParams() {
+export default function stripOAuthParams() {
   try {
     const url = new URL(window.location.href);
     let changed = false;
+
     ["code", "state", "provider", "error", "error_description"].forEach((p) => {
       if (url.searchParams.has(p)) {
         url.searchParams.delete(p);
         changed = true;
       }
     });
+
     if (url.hash && /access_token|refresh_token|error/i.test(url.hash)) {
       url.hash = "";
       changed = true;
@@ -16,5 +17,5 @@ export function stripOAuthParams() {
     if (changed) {
       window.history.replaceState({}, document.title, url.toString());
     }
-  } catch {}
+  } catch {/* noop */}
 }
