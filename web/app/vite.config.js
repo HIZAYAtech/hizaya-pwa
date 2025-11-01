034ca3ab82e4cc1d4c95d74f08abb1d5b518ad1d
HIZAYAtech <164567835+HIZAYAtech@users.noreply.github.com>
2025-11-01 15:46:27 +0100

    Update vite.config.js

--- web/app/vite.config.js
+++ web/app/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hizaya-pwa/"
-});
+  base: process.env.VITE_BASE || '/',   // <- par défaut en dev
+  build: { assetsDir: 'assets' }
+})
